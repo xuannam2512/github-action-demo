b64a20780170e7c1d963e2ea82b80f63b1eee1ae
Nam Le <nam.le@finbase.vn>
2022-01-25 12:24:47 +0700

    Update config

--- src/main/java/com/gihubaction/githubactiondemo/controller/TestController.java
+++ src/main/java/com/gihubaction/githubactiondemo/controller/TestController.java
@@ -4,7 +4,7 @@ import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "test")
+@RequestMapping(value = "/api/test")
 public class TestController {
 
   @GetMapping
