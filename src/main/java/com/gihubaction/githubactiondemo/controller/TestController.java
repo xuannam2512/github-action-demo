fe92f0c1fffab92d66a9c45b8236b21531d4b329
Nam Le <nam.le@finbase.vn>
2022-01-25 17:21:09 +0700

    Update code change

--- src/main/java/com/gihubaction/githubactiondemo/controller/TestController.java
+++ src/main/java/com/gihubaction/githubactiondemo/controller/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
 
   @GetMapping
   public ResponseEntity<String> test() {
-    return ResponseEntity.ok().body("Hello world haha");
+    return ResponseEntity.ok().body("Hello world Re-test");
   }
 }
